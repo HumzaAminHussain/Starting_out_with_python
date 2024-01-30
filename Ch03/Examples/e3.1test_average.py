150cf1700a5e4acb52c4ea1222be920b54a85cb4
HumzaAminHussain <156113931+HumzaAminHussain@users.noreply.github.com>
2024-01-30 12:19:12 -0500

    Update e3.1test_average.py

--- Ch03/Examples/e3.1test_average.py
+++ Ch03/Examples/e3.1test_average.py
@@ -2,7 +2,7 @@ HIGH_SCORE = 95
 
 test1 = int(input('Enter the score for test 1: '))
 test2 = int(input('Enter the score for test 2: '))
-test3 = int(input('Enter the score for test 2: '))
+test3 = int(input('Enter the score for test 3: '))
 
 avarage = (test1 + test2 + test3) / 3
 print(f'the avarage test score is {avarage:.2f}.')
