e72092493c053630671ddc4f6da3399e571d0860
HumzaAminHussain <156113931+HumzaAminHussain@users.noreply.github.com>
2024-02-15 12:21:32 -0500

    Name sorter
    this will sort your names alphabetically

---
+++ Ch03/Examples/e3.4sort_names copy.py
@@ -0,0 +1,16 @@
+flag =  True
+while flag:
+    name1 = input('Enter a name (last name first): ')
+    name2 = input('Enter another name (last name first): ')
+    if name1 < name2:
+        print('Here are the name, listed alphabetically.')
+        print(name1)
+        print(name2)
+        flag = False
+    elif name2 < name1:
+        print('Here are the name, listed alphabetically.')
+        print(name2)
+        print(name1)
+        flag = False
+    else:
+        print('The names are the same, please put different names.')
---
+++ Ch03/Examples/e3.4sort_names.py
@@ -0,0 +1,9 @@
+name1 = input('Enter a name (last name first): ')
+name2 = input('Enter another name (last name first): ')
+print('Here are the name, listed alphabetically.')
+if name1< name2:
+    print(name1)
+    print(name2)
+else:
+    print(name2)
+    print(name2)
