43d1fd8b34243cbc4a74212dd7bfaf4e6ff61d17
HumzaAminHussain <156113931+HumzaAminHussain@users.noreply.github.com>
2024-03-04 08:42:32 -0500

    Update pe3.02AreasOfRectangles.py

--- Ch03/Programming_Exercises/pe3.02AreasOfRectangles.py
+++ Ch03/Programming_Exercises/pe3.02AreasOfRectangles.py
@@ -5,8 +5,8 @@ rec2length = float(input('Enter the length of rectangle 2: '))
 rec1 = rec1with*rec1length
 rec2 = rec2length*rec1with
 if rec1 > rec2:
-    print('Rectangle 1\'s area is greater than rectangle 2\'s area')
+    print('Rectangle 1\'s area is greater than rectangle 2\'s area.')
 elif rec2 > rec1:
-    print('Rectangle 2\'s area is greater than rectangle 1\'s area')
+    print('Rectangle 2\'s area is greater than rectangle 1\'s area.')
 else:
     print('The areas of both rectangles are the same.')
