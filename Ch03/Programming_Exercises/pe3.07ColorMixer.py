f042cb5b9bcd3e02c800f5b166f2048662e66156
HumzaAminHussain <156113931+HumzaAminHussain@users.noreply.github.com>
2024-03-05 12:09:44 -0500

    Update pe3.07ColorMixer.py

--- Ch03/Programming_Exercises/pe3.07ColorMixer.py
+++ Ch03/Programming_Exercises/pe3.07ColorMixer.py
@@ -2,21 +2,21 @@ color1 = input('Enter a color that is either red, yellow, or blue (all lowercase
 color2 = input('Enter a different color that is either red, yellow, or blue (all lowercase): ')
 if color1 == "red" and color2 == "yellow":
     print('Orange.')
-if color1 == "red" and color2 == "blue":
+elif color1 == "red" and color2 == "blue":
     print('Purple.')
-if color1 == "blue" and color2 == "red":
+elif color1 == "blue" and color2 == "red":
     print('Purple.')
-if color1 == "blue" and color2 == "yellow":
+elif color1 == "blue" and color2 == "yellow":
     print("Green.")
-if color1 == "yellow" and color2 == "blue":
+elif color1 == "yellow" and color2 == "blue":
     print("Green.")
-if color1 == "yellow" and color2 == "red":
+elif color1 == "yellow" and color2 == "red":
     print("Orange.")
-if color1 == "yellow" and color2 == "yellow":
+elif color1 == "yellow" and color2 == "yellow":
     print("Yellow.")
-if color1 == "red" and color2 == "red":
+elif color1 == "red" and color2 == "red":
     print("Red.")
-if color1 == "blue" and color2 == "blue":
-    print("blue.")
+elif color1 == "blue" and color2 == "blue":
+    print("Blue.")
 else:
     print("ERROR: you entered somthing other than the primary colors.")
