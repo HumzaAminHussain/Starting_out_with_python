73c3e8a1b2240bf2dfac95a886df5ccb8ade4960
HumzaAminHussain <156113931+HumzaAminHussain@users.noreply.github.com>
2024-03-19 09:28:46 -0400

    Update pe3.09RouletteWheelColors.py

--- Ch03/Programming_Exercises/pe3.09RouletteWheelColors.py
+++ Ch03/Programming_Exercises/pe3.09RouletteWheelColors.py
@@ -7,7 +7,7 @@ if num <0 or num >36:
     print(f'ERROR: You entered {num}, a number outside the range 0-36.')
 elif num == 0:
     print(p0)
-elif (num >= 1 and num <= 10) or (num >= 19 and num <= 29):
+elif (num >= 1 and num <= 10) or (num >= 19 and num <= 28):
     if num%2 == 0:
         print(p2)
     else:
