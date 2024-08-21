2ee20bc3e93daae569002bff4e0b2a0d11b2ce9c
HumzaAminHussain <156113931+HumzaAminHussain@users.noreply.github.com>
2024-08-21 09:17:12 -0400

    Update caterpiller.py

--- caterpiller.py
+++ caterpiller.py
@@ -3,7 +3,7 @@ import turtle as t
 
 t.bgcolor('yellow')
 caterpillar = t.Turtle()
-caterpillar.shape('triangle')
+caterpillar.shape('square')
 caterpillar.color('red')
 caterpillar.speed(0)
 caterpillar.penup()
